f3873cc88aa73284359327fd8f79d05c47fcf44c
silver <noahcy117@gmail.com>
2018-01-02 22:57:21 -0600

    materialize added

--- static/main.js
+++ static/main.js
@@ -1,1 +1,23 @@
 console.log("Hello");
+var $body = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+
+$('#sidebar').affix({
+      offset: {
+        /* affix after top masthead */
+        top: function () {
+            var navOuterHeight = $('#masthead').height();
+            return this.top = navOuterHeight;
+        },
+        /* un-affix when footer is reached */
+        bottom: function () {
+            return (this.bottom = $('footer').outerHeight(true))
+        }
+      }
+});
+
+$body.scrollspy({
+	target: '#leftCol',
+	offset: navHeight
+});
